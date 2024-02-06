1d3e7228b0fcdca29e2c699f20f2198d3977d43b
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-02-06 17:12:01 +0100

    Se connecter à Mongo DB via Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from './config';
 
 @Module({
-  imports: [ArticlesModule],
+  imports: [ArticlesModule, MongooseModule.forRoot(config.mongoUri)],
   controllers: [AppController],
   providers: [AppService],
 })
